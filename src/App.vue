45c00c4a81e339c69a1e572f76015d4a0154ae64
Lynn-zuo <1528174573@qq.com>
2020-08-06 17:32:57 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Lynn></Lynn>
+</template>
+
+<script>
+import Lynn from './components/Lynn'
+
+export default {
+  name: 'App',
+  components: {
+    Lynn
+  }
+}
+</script>
