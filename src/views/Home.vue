4a3061dfc0ef08cdffc0792af5b4672315f21aa3
Lynn-zuo <1528174573@qq.com>
2020-08-06 22:13:47 +0800

    add banner-css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,10 +3,10 @@
     <TopNav />
     <div class="banner">
       <h1><a class="Doc-title" href="">AppLe-UI</a></h1>
-      <h3>一个UI</h3>
+      <h3>一个优点可爱的简洁UI</h3>
       <p class="actions">
-        <a href="">GitHub</a>
-        <a href="">开始</a>
+        <a href="https://github.com/Lynn-zuo" target="_blank">GitHub</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
@@ -22,12 +22,31 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$h:1.6rem;
 .banner {
-  margin-top: 2rem;
-  .actions a{
-    &:hover {
-      color: #3366FF;
-      border-bottom: 1px solid #3333FF;
+  background: #CCFFFF;
+  padding-top: 2rem;
+  display:flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 20rem;
+  h1 {
+    margin: 1rem 0;
+  }
+  > .actions {
+    padding:1rem 0;
+    a{
+      margin: 1rem;
+      background: #fff;
+      display: inline-block;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 1rem;
+      &:hover {
+        color: #3366FF;
+      }
     }
   }
 }
