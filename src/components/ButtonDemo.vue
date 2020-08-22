117934ae010dda15c8c18ec4e2f72955a42ba59e
Lynn-zuo <1528174573@qq.com>
2020-08-22 20:06:00 +0800

    add theme&size&level&disabled&loading properties

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -5,13 +5,88 @@
   <h1>示例1</h1>
   <div>
     <Button @click="onClick"
-    >你好</Button>
-    <Button @click="onClick" theme="button"
-    >你好</Button>
-    <Button @click="onClick" theme="link"
-    >你好</Button>
-    <Button @click="onClick" theme="text"
-    >你好</Button>
+    >默认</Button>
+    <Button theme="button"
+    >按钮</Button>
+    <Button theme="link"
+    >链接</Button>
+    <Button theme="text"
+    >文本</Button>
+  </div>
+  <h1>示例2</h1>
+  <div>
+    <Button @click="onClick"
+    >默认</Button>
+    <Button size="big"
+    >大号</Button>
+    <Button size="small"
+    >小号</Button>
+  </div>
+  <div>
+    <Button theme="link"
+    >默认</Button>
+    <Button theme="link" size="big"
+    >大号</Button>
+    <Button theme="link" size="small"
+    >小号</Button>
+  </div>
+  <div>
+    <Button theme="text"
+    >默认</Button>
+    <Button theme="text" size="big"
+    >大号</Button>
+    <Button theme="text" size="small"
+    >小号</Button>
+  </div>
+  <h1>示例3</h1>
+  <div>
+    <Button @click="onClick"
+    >普通按钮</Button>
+    <Button level="main"
+    >主要按钮</Button>
+    <Button level="danger"
+    >危险按钮</Button>
+  </div>
+  <div>
+    <Button theme="link"
+    >普通链接</Button>
+    <Button theme="link" level="main"
+    >主要链接</Button>
+    <Button theme="link" level="danger"
+    >危险链接</Button>
+  </div>
+  <div>
+    <Button theme="text"
+    >普通文字按钮</Button>
+    <Button theme="text" level="main"
+    >主要文字按钮</Button>
+    <Button theme="text" level="danger"
+    >危险文字按钮</Button>
+  </div>
+  <h1>示例4</h1>
+  <div>
+    <Button disabled
+    >禁用按钮</Button>
+    <Button :disabled="true"
+    >禁用按钮</Button>
+  </div>
+  <div>
+    <Button theme="text"  disabled
+    >禁用按钮</Button>
+    <Button theme="text"  :disabled="true"
+    >禁用按钮</Button>
+  </div>
+  <div>
+    <Button theme="text" disabled
+    >禁用按钮</Button>
+    <Button theme="text"  :disabled="true"
+    >禁用按钮</Button>
+  </div>
+  <h1>示例5</h1>
+  <div>
+    <Button loading
+    >加载中</Button>
+    <Button>加载完毕</Button>
   </div>
 </template>
 
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,26 +1,59 @@
 <template>
-  <button class="app-button" :class="`apple-theme-${theme}`">
-    <slot></slot>
+  <button class="apple-button" 
+  :class="classes" 
+  :disabled="disabled">
+    <span class="apple-loadingIndicator" v-if="loading"></span>
+    <slot/>
   </button>
 </template>
 
 <script lang="ts">
+import { computed } from 'vue'
 export default {
-  props: {theme:{
-    type: String,
-    default: 'button'
-  }},
-  setup(props, context) {
+  props: {
+    theme:{
+      type: String,
+      default: 'button'
+    },
+    size:{
+      type: String,
+      default: "normal"
+    },
+    level:{
+      type: String,
+      default: "normal"
+    },
+    disabled:{
+      type: Boolean,
+      default: false
+    },
+    loading:{
+      type: Boolean,
+      default: false
+    }
+  },
+  setup(props) {
+    const {theme, size, level} = props
+    const classes = computed (() => {
+      return {
+        [`apple-theme-${theme}`]:theme,
+        [`apple-size-${size}`]:size,
+        [`apple-level-${level}`]:level
+      }
+    })
+    return {classes}
   }
 }
 </script>
 <style lang="scss">
-$h: 32px;
+$h: 28px;
 $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
+$red: #f50808;
+$grey: rgb(125, 126, 126);
 $radius: 4px;
-.app-button {
+.apple-button {
   box-sizing: border-box;
   height: $h;
   padding: 0 12px;
@@ -47,5 +80,94 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+  &.apple-theme-link{
+    border-color: transparent;
+    background: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,&:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+  &.apple-theme-text{
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    background: transparent;
+    &:hover,&:focus {
+      background: darken(white, 5%);
+    }
+  }
+  &.apple-size-small{
+    font-size: 8px;
+    height: 24px;
+  }
+  &.apple-size-big{
+    font-size: 24px;
+    height: 36px;
+  }
+  &.apple-theme-button{
+    &.apple-level-main{
+      color:white;
+      background: $blue;
+      &:hover,&:focus{
+        color: darken($blue, 10%);
+      }
+    }
+    &.apple-level-danger{
+      background: $red;
+      &:hover,&:focus{
+        background: darken($red, 10%);
+        color: $color;
+        border-color: transparent;
+      }
+    }
+  }
+  &.apple-theme-link, &.apple-theme-text {
+    &.apple-level-main{
+      color: $blue;
+      &:hover,&:focus{
+        color: darken($blue, 10%);
+      }
+    }
+    &.apple-level-danger{
+      color: $red;
+      &:hover,&:focus{
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.apple-theme-button{
+    &[disabled]{
+      cursor: not-allowed;
+      color:$grey;
+      background: transparent;
+      &:hover,&:focus{
+        border-color:$grey;
+      }
+    }
+  }
+  &.apple-theme-link, &.apple-theme-text {
+    &[disabled]{
+      cursor: not-allowed;
+      color:$grey;
+      background: transparent;
+    }
+  }
+  > .apple-loadingIndicator {
+    width:14px;
+    height:14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 7px;
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: apple-spin 1s infinite linear;
+  }
+}
+@keyframes apple-spin {
+  0%{transform: rotate(0deg)}
+  100%{transform: rotate(360deg)}
 }
 </style>
